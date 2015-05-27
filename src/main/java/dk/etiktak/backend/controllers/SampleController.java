aadc3f8a8dc99b67695cf0bc55e2a066ce4b2b0e
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-05-27 21:58:22 +0200

    Rest controller

--- src/main/java/dk/etiktak/backend/controllers/SampleController.java
+++ src/main/java/dk/etiktak/backend/controllers/SampleController.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class SampleController {
 
