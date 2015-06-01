2e5c7bdaa4b3f45df84fe96f27e81d8b0605c95a
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-06-01 22:19:14 +0200

    Comment

--- src/main/java/dk/etiktak/backend/controllers/BaseController.java
+++ src/main/java/dk/etiktak/backend/controllers/BaseController.java
@@ -2,6 +2,9 @@ package dk.etiktak.backend.controllers;
 
 import dk.etiktak.backend.controllers.json.BaseJsonObject;
 
+/**
+ * Used as a parent for all controllers.
+ */
 public class BaseController {
     public BaseJsonObject ok() {
         return new BaseJsonObject();
