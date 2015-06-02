532e79e6e92e6c84a701aa0398c46653c17ba7ee
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-06-02 21:01:00 +0200

    Minor refactor

--- src/main/java/dk/etiktak/backend/service/user/UserService.java
+++ src/main/java/dk/etiktak/backend/service/user/UserService.java
@@ -5,10 +5,12 @@ import dk.etiktak.backend.model.user.MobileNumber;
 import dk.etiktak.backend.model.user.SmsVerification;
 
 public interface UserService {
-    void createUser(String mobileNumber, String password) throws Exception;
-    void verifySmsChallenge(String mobileNumber, String password, String smsChallenge, String clientChallenge) throws Exception;
+    void createUser(String username, String password) throws Exception;
+
+    Client createClient(String mobileNumber, String password) throws Exception;
 
     MobileNumber createMobileNumber(String mobileNumber) throws Exception;
-    Client createClient(String mobileNumber, String password) throws Exception;
+
     SmsVerification sendSmsChallenge(String mobileNumber) throws Exception;
+    void verifySmsChallenge(String mobileNumber, String password, String smsChallenge, String clientChallenge) throws Exception;
 }
