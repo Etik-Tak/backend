3e0356600a0f1cbf086f10c9800e5788075420ec
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-06-02 21:00:03 +0200

    User model and repository

---
+++ src/main/java/dk/etiktak/backend/model/user/User.java
@@ -0,0 +1,91 @@
+package dk.etiktak.backend.model.user;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "uuid", nullable = false, unique = true)
+    private String uuid;
+
+    @Column(name = "usernameHash", nullable = false, unique = true)
+    private String usernameHash;
+
+    @Column(name = "passwordHash", nullable = false)
+    private String passwordHash;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date creationTime;
+
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date modificationTime;
+
+    public User() {}
+
+    @PreUpdate
+    public void preUpdate() {
+        modificationTime = new Date();
+    }
+
+    @PrePersist
+    public void prePersist() {
+        Date now = new Date();
+        creationTime = now;
+        modificationTime = now;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUsernameHash() {
+        return usernameHash;
+    }
+
+    public void setUsernameHash(String usernameHash) {
+        this.usernameHash = usernameHash;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Date getModificationTime() {
+        return modificationTime;
+    }
+
+    public void setModificationTime(Date modificationTime) {
+        this.modificationTime = modificationTime;
+    }
+}
