782eb536eb8a56dac3710ac313afa26184f3d98a
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-06-02 21:00:52 +0200

    New query method

--- src/main/java/dk/etiktak/backend/repository/user/ClientRepository.java
+++ src/main/java/dk/etiktak/backend/repository/user/ClientRepository.java
@@ -7,5 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
+    Client findByUuid(String uuid);
     Client findByMobileNumberHashPasswordHashHashed(String mobileNumberHashedPaswordHashedHashed);
 }
