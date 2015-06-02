662cbce14182f76c7f56dfdf3a233cd35a98531c
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-06-02 20:59:37 +0200

    Initial web security

---
+++ src/main/java/dk/etiktak/backend/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package dk.etiktak.backend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
+@Configuration
+@EnableWebMvcSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final String[] unprotectedUrls = {"/", "/service/**"};
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers(unprotectedUrls).permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+            .logout()
+                .permitAll();
+    }
+}
