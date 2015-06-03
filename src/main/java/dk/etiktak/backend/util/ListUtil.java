d1b3cbd4dce45e11f77fbdce46d5568fd0d0d788
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-06-03 20:44:38 +0200

    Name refactor

--- src/main/java/dk/etiktak/backend/util/ListUtil.java
+++ src/main/java/dk/etiktak/backend/util/ListUtil.java
@@ -3,7 +3,7 @@ package dk.etiktak.backend.util;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Lists {
+public class ListUtil {
     public static <E> List<E> asList(Iterable<E> iter) {
         List<E> list = new ArrayList<E>();
         for (E item : iter) {
