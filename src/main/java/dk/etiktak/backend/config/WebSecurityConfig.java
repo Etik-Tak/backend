c6277caf21bf9e1a16cceffa596d50f15fbca31f
Daniel Andersen <daniel.andersen@alexandra.dk>
2015-12-12 20:57:37 +0100

    Enable (disabled) spring security ;)

--- src/main/java/dk/etiktak/backend/config/WebSecurityConfig.java
+++ src/main/java/dk/etiktak/backend/config/WebSecurityConfig.java
@@ -43,15 +43,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+            .csrf()
+                .disable()
             .authorizeRequests()
-                .antMatchers(unprotectedUrls).permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .permitAll();
+                .antMatchers(unprotectedUrls).permitAll();
     }
 }
