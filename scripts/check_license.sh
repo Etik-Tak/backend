dc3d299b0476fe7155968627c8d8675a52c74332
Daniel Andersen <daniel.andersen@alexandra.dk>
2017-01-12 12:18:58 +0100

    Updated license year

--- scripts/check_license.sh
+++ scripts/check_license.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -name "*.java" | xargs grep -L "Copyright (c) 2015, Daniel Andersen (daniel@trollsahead.dk)"
-find . -name "*.kt" | xargs grep -L "Copyright (c) 2015, Daniel Andersen (daniel@trollsahead.dk)"
+find . -name "*.java" | xargs grep -L "Copyright (c) 2017, Daniel Andersen (daniel@trollsahead.dk)"
+find . -name "*.kt" | xargs grep -L "Copyright (c) 2017, Daniel Andersen (daniel@trollsahead.dk)"
